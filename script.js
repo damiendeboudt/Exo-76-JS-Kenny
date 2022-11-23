a2d373ccb46cc2ef1d2417ca71d8aa9ceb63aa90
Damien <damien.deboudt@sfr.fr>
2022-11-23 11:02:11 +0100

    finish

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
-
+let x = 1;
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(document.getElementById('kenny').style.top);
     t = t - 10;
     if (t >= 0 )
     document.getElementById('kenny').style.top = t + 'px';
-    //else if
 });
 
 document.getElementById('left').addEventListener('click',function() {
@@ -13,14 +12,13 @@ document.getElementById('left').addEventListener('click',function() {
     t = t - 10;
     if (t >= 0 )
     document.getElementById('kenny').style.left = t + 'px';
+    //let x = 0;
     if ( t <= 40) {
         alert("Kenny est mort")
         document.getElementById("kenny").style.left = 200 + "px";
         document.getElementById("kenny").style.top = 200 + "px";
-        let x = 1
-        let killed = (t <= 40)
-        let xkilled = x * killed
-        alert(" kenny est mort" + xkilled + "fois")
+        let count = x++
+        alert(" kenny est mort " + count + " fois")
     }
 });
 
@@ -39,10 +37,6 @@ document.getElementById('down').addEventListener('click',function() {
 });
 
 
-
-
-
-
 let view = document.getElementById("viewport");
 let zoneRouge = document.createElement("div");
 zoneRouge.style.backgroundColor = "red";
@@ -51,3 +45,37 @@ zoneRouge.style.width = "50px";
 zoneRouge.style.height = "500px";
 view.append(zoneRouge);
 
+document.addEventListener("keypress", function (p) {
+    if (p.key === "z") {
+        let t = parseInt(document.getElementById('kenny').style.top);
+        t = t - 10;
+        if (t >= 0 )
+            document.getElementById('kenny').style.top = t + 'px';
+    }
+    else if (p.key === "q") {
+        let t = parseInt(document.getElementById('kenny').style.left);
+        t = t - 10;
+        if (t >= 0 )
+            document.getElementById('kenny').style.left = t + 'px';
+        //let x = 0;
+        if ( t <= 40) {
+            alert("Kenny est mort")
+            document.getElementById("kenny").style.left = 200 + "px";
+            document.getElementById("kenny").style.top = 200 + "px";
+            let count = x++
+            alert(" kenny est mort " + count + " fois")
+        }
+    }
+    else if (p.key === "d") {
+        let t = parseInt(document.getElementById('kenny').style.left);
+        t = t + 10;
+        if ( t <= 470)
+            document.getElementById('kenny').style.left = t + 'px';
+    }
+    else if (p.key === "s") {
+        let t = parseInt(document.getElementById('kenny').style.top);
+        t = t + 10;
+        if ( t <= 470)
+            document.getElementById('kenny').style.top = t + 'px';
+    }
+})
